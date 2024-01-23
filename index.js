31497bbb505fc4e3fb48cf211084db2692aabd1a
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-23 15:47:33 +0200

    created customers table

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'qwerty'
+    password: 'qwerty',
+    database: "mydb"
 });
 
 con.connect(err => {
     if (err) throw err;
     console.log('Connected');
 
-    con.query("create database mydb", (err, result) => {
+    const sql = "create table customers (name varchar(255), address varchar(255))";
+    con.query(sql, (err, result) => {
         if (err) throw err;
-        console.log(result);
+        console.log(result.affectedRows);
     });
 });
 
